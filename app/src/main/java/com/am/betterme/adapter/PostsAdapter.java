72e6fcb54609f3fff06830a7b301494b657dbcae
Abed Murad <abdallah.Murad@mail.com>
2019-01-09 00:36:46 +0100

    Finish PostsAdapter

--- app/src/main/java/com/am/betterme/adapter/PostsAdapter.java
+++ app/src/main/java/com/am/betterme/adapter/PostsAdapter.java
@@ -1,6 +1,7 @@
 package com.am.betterme.adapter;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,13 +9,12 @@ import android.view.ViewGroup;
 
 
 import com.am.betterme.data.Post;
-import com.am.betterme.databinding.CardArticleBinding;
+import com.am.betterme.databinding.CardPostBinding;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
-public class PostsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostHolder> {
     private static final int TYPE_ARTICLE = 1;
     private static final int TYPE_VIDEO = 2;
 
@@ -33,52 +33,49 @@ public class PostsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        switch (viewType) {
-            case TYPE_ARTICLE: {
-                CardArticleBinding cardBinding = CardArticleBinding.inflate(mInflater, parent, false);
-                return new ViewHolder(cardBinding);
-            }
-        }
-        return null;
+    public PostHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                CardPostBinding cardBinding = CardPostBinding.inflate(mInflater, parent, false);
+                return new PostHolder(cardBinding);
+
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull PostHolder postHolder, final int position) {
 
-        if (holder instanceof ViewHolder && mPostList != null) {
             if (mPostList.size() != 0) {
-                final Article article = getItem(position);
-                ViewHolder viewHolder = (ViewHolder) holder;
-                viewHolder.bindData(article);
+
+                if (position % 2 == 0) {
+                    postHolder.mBinding.imageView2.setVisibility(View.INVISIBLE);
+                } else {
+                    postHolder.mBinding.imageView2.setVisibility(View.VISIBLE);
+                }
+                final Post post = getItem(position);
+                postHolder.bindData(post);
+
             }
 
-        }
+
     }
 
-    @Override
-    public int getItemViewType(int position) {
-        return TYPE_ARTICLE;
-    }
+
 
     @Override
     public int getItemCount() {
-        return mPostList == null ? 0 : mPostList.size();
+        return mPostList == null ? 10 : mPostList.size();
     }
 
-    public void add(Article article) {
-        mPostList.add(article);
+    public void add(Post post) {
+        mPostList.add(post);
         notifyItemInserted(mPostList.size());
     }
 
-    public void addAll(List<Article> articleList) {
-        for (Article article : articleList) {
-            add(article);
-            Logger.d(article);
+    public void addAll(List<Post> postList) {
+        for (Post post : postList) {
+            add(post);
         }
     }
 
-    private Article getItem(int position) {
+    private Post getItem(int position) {
         return mPostList.get(position);
     }
 
@@ -87,26 +84,22 @@ public class PostsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     }
 
     public interface OnArticleClickListener {
-        //TODO: use this method in the real project data not the parameter-less onItemClick()
-        void onItemClick(View view, int position, Article model);
-
-        void onBookmarkButtonClick(Article model);
+        void onItemClick(View view, int position, Post model);
     }
 
     public class PostHolder extends RecyclerView.ViewHolder {
-        private final  mBinding;
+        private final CardPostBinding mBinding;
 
-        public ViewHolder(CardFeedArticleBinding itemView) {
+        PostHolder(CardPostBinding itemView) {
             super(itemView.getRoot());
             this.mBinding = itemView;
-            mBinding.getRoot().setOnClickListener(view -> mArticleClickListener.onItemClick(mBinding.getRoot(), getAdapterPosition(), getItem(getAdapterPosition())));//TODO: Remove this Dummy Data
-            mBinding.addToFavoriteFab.setOnClickListener(view -> mArticleClickListener.onBookmarkButtonClick(getItem(getAdapterPosition())));//TODO: Remove this Dummy Data
+            mBinding.getRoot().setOnClickListener(view -> mArticleClickListener.onItemClick(mBinding.getRoot(), getAdapterPosition(), getItem(getAdapterPosition())));
 
         }
 
-        private void bindData(Article article) {
-            mBinding.setArticle(article);
-            Glide.with(mContext).load(article.getUrlToImage()).into(mBinding.articleImage);
+        private void bindData(Post post) {
+//            mBinding.setPost(post);
+//            Glide.with(mContext).load(post.getUrlToImage()).into(mBinding.articleImage);
             mBinding.executePendingBindings();
         }
     }

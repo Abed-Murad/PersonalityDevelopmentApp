c7c8a4d6dd6341e378c3b28a5e42abc44bbc4d96
Abed Murad <abdallah.Murad@mail.com>
2019-01-08 05:35:18 +0100

    Activite DataBinding

--- app/src/main/java/com/am/personalitydevelopmentapp/activity/SplashActivity.java
+++ app/src/main/java/com/am/personalitydevelopmentapp/activity/SplashActivity.java
@@ -1,12 +1,9 @@
 package com.am.personalitydevelopmentapp.activity;
 
-import android.app.AppComponentFactory;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
-import com.am.personalitydevelopmentapp.MainActivity;
 
 public class SplashActivity extends AppCompatActivity {
 

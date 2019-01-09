b5f8e274b7fbc30d28465d71674191a4255b18f5
Abed Murad <abdallah.Murad@mail.com>
2019-01-09 01:47:32 +0100

    Fix PostsFragmentLists Bugs

--- app/src/main/java/com/am/betterme/fragment/PostsListFragment.java
+++ app/src/main/java/com/am/betterme/fragment/PostsListFragment.java
@@ -1,6 +1,7 @@
 package com.am.betterme.fragment;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -21,11 +22,13 @@ import android.widget.Toast;
 import com.am.betterme.R;
 import com.am.betterme.adapter.PostsAdapter;
 import com.am.betterme.data.PostsListViewModel;
+import com.am.betterme.databinding.PostsListFragmentBinding;
 
 public class PostsListFragment extends Fragment {
 
     private PostsListViewModel mViewModel;
-
+    private PostsListFragmentBinding mLayout;
+    private Context mContext;
     public static PostsListFragment newInstance() {
         return new PostsListFragment();
     }
@@ -33,10 +36,11 @@ public class PostsListFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        mContext = getContext();
+        mLayout = PostsListFragmentBinding.inflate(inflater, container, false);
         setupTabLayout();
         setupRecyclerView();
-
-        return inflater.inflate(R.layout.posts_list_fragment, container, false);
+        return mLayout.getRoot();
     }
 
     @Override
@@ -51,23 +55,23 @@ public class PostsListFragment extends Fragment {
 
 
     private void setupRecyclerView() {
-        RecyclerView postsRecyclerView = findViewById(R.id.postsRecyclerView);
-        postsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        postsRecyclerView.setAdapter(new PostsAdapter(this, (view, position, model) -> {
-            Toast.makeText(this, "Fuck it " + position, Toast.LENGTH_SHORT).show();
+        RecyclerView postsRecyclerView = mLayout.postsRecyclerView;
+        postsRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+        postsRecyclerView.setAdapter(new PostsAdapter(mContext, (view, position, model) -> {
+            Toast.makeText(mContext, "Fuck it " + position, Toast.LENGTH_SHORT).show();
         }));
     }
 
     private void setupTabLayout() {
-        TabLayout tabLayout = findViewById(R.id.tagsTabLayout);
+        TabLayout tabLayout = mLayout.tagsTabLayout;
         tabLayout.addTab(tabLayout.newTab());
-        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.tab_tag, null);
+        TextView tabOne = (TextView) LayoutInflater.from(mContext).inflate(R.layout.tab_tag, null);
         tabOne.setText("All");
         tabLayout.getTabAt(0).setCustomView(tabOne);
 
         for (int i = 1; i < 10; i++) {
             tabLayout.addTab(tabLayout.newTab());
-            TextView tab = (TextView) LayoutInflater.from(this).inflate(R.layout.tab_tag, null);
+            TextView tab = (TextView) LayoutInflater.from(mContext).inflate(R.layout.tab_tag, null);
             tab.setText("Self Steam");
             tabLayout.getTabAt(i).setCustomView(tab);
         }

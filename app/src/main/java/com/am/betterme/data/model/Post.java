bd15ea36df7f1ec9fc4024c45152d2f29113ac13
Abed Murad <abdallah.Murad@mail.com>
2019-01-17 13:21:11 +0200

    Refactor Post Model

--- app/src/main/java/com/am/betterme/data/model/Post.java
+++ app/src/main/java/com/am/betterme/data/model/Post.java
@@ -5,8 +5,6 @@ import android.os.Parcelable;
 
 import com.am.betterme.util.FUNC;
 
-import org.ocpsoft.prettytime.PrettyTime;
-
 import java.util.Date;
 import java.util.List;
 
@@ -20,28 +18,19 @@ public class Post implements Parcelable {
     private boolean is_video;
     private List<String> tags;
 
+    public Post() {}
+
     public String getBody() {
         return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
     }
 
     public String getDate() {
         return FUNC.getPrettyDate(date);
     }
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
 
     public String getImage_url() {
         return image_url;
-    }
-
-    public void setImage_url(String image_url) {
-        this.image_url = image_url;
     }
 
     public String getTitle() {
@@ -52,33 +41,10 @@ public class Post implements Parcelable {
         this.title = title;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 
     public boolean isIs_video() {
         return is_video;
     }
-
-    public void setIs_video(boolean is_video) {
-        this.is_video = is_video;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
-    }
-
-    public Post() {
-    }
-
 
 
     @Override

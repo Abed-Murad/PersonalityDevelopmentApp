48d05f37c29033b831d357149884a63ca57ca03a
Abed Murad <abdallah.Murad@mail.com>
2019-01-17 13:38:07 +0200

    Refactor PostsFragment

--- app/src/main/java/com/am/betterme/fragment/PostsFragment.java
+++ app/src/main/java/com/am/betterme/fragment/PostsFragment.java
@@ -3,21 +3,18 @@ package com.am.betterme.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.am.betterme.R;
 import com.am.betterme.adapter.PostsAdapter;
 import com.am.betterme.data.model.Post;
 import com.am.betterme.data.viewmodel.PostsListViewModel;
 import com.am.betterme.databinding.PostsFragmentBinding;
-import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.orhanobut.logger.Logger;
 
 import java.util.List;
 
@@ -27,8 +24,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import me.gujun.android.taggroup.TagGroup;
+import static com.am.betterme.util.CONST.POSTS_KEY;
+import static com.am.betterme.util.CONST.TAGS_ARRAY;
 
 public class PostsFragment extends Fragment {
 
@@ -36,41 +33,26 @@ public class PostsFragment extends Fragment {
     private PostsFragmentBinding mLayout;
     private Context mContext;
     private PostsAdapter mPostsAdapter;
-    private CollectionReference mPostRef = FirebaseFirestore.getInstance().collection("Posts");
-    private List<String> tagsList;
+    private CollectionReference mPostRef = FirebaseFirestore.getInstance().collection(POSTS_KEY);
 
-
-    public static PostsFragment newInstance() {
-        return new PostsFragment();
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getContext();
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        mContext = getContext();
-
-//        mPostRef.whereEqualTo("is_video", true).get().addOnCompleteListener(task -> {
-        mPostRef.get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                List<Post> postList = task.getResult().toObjects(Post.class);
-                mPostsAdapter.addAll(postList);
-
-            } else {
-                Log.w("ttt", "Error getting documents.", task.getException());
-            }
-        });
         mLayout = PostsFragmentBinding.inflate(inflater, container, false);
         setupRecyclerView();
         setupTabLayout();
-
-
         return mLayout.getRoot();
     }
 
     //TODO: FIX The OnClick Bug
     private void setupTabLayout() {
-        String[] array = {"All","Relationships", "Motivation", "Social Skills", "Goal Setting", "Habit Building", "Career", "Health", "Family"};
-        mLayout.tagsTabLayout.setTags(array);
+        mLayout.tagsTabLayout.setTags(TAGS_ARRAY);
         mLayout.tagsTabLayout.setOnTagClickListener(tag -> Toast.makeText(getActivity(), tag, Toast.LENGTH_SHORT).show());
     }
 
@@ -84,18 +66,24 @@ public class PostsFragment extends Fragment {
 
 
     private void setupRecyclerView() {
-
-        RecyclerView postsRecyclerView = mLayout.postsRecyclerView;
-        postsRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+        mLayout.postsRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
         mPostsAdapter = new PostsAdapter(mContext, (view, position, model) -> {
             PostsFragmentDirections.ActionPostsFragmentToPostDetailsFragment action = PostsFragmentDirections.actionPostsFragmentToPostDetailsFragment();
-            action.setPostId(position + "");
             action.setPost(model);
             Navigation.findNavController(view).navigate(action);
-
         });
-        postsRecyclerView.setAdapter(mPostsAdapter);
+        mLayout.postsRecyclerView.setAdapter(mPostsAdapter);
+        mPostRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                List<Post> postList = task.getResult().toObjects(Post.class);
+                mPostsAdapter.addAll(postList);
+
+            } else {
+                Logger.e("Error getting documents.", task.getException());
+            }
+        });
     }
 //TODO: Create A Listener here to ne implemented by the main Activity so it can change the Fragment Data When ever this drawer button is clicked
+//TODO: mPostRef.whereEqualTo("is_video", true).get().addOnCompleteListener(task -> {
 
 }
--- app/src/main/java/com/am/betterme/util/CONST.java
+++ app/src/main/java/com/am/betterme/util/CONST.java
@@ -3,5 +3,6 @@ package com.am.betterme.util;
 public  interface CONST {
     String BASE_YOUTUBE_URL = "http://www.youtube.com/watch?v=";
     String TEST_VIDEO_ID = "wA38GCX4Tb0";
+    String POSTS_KEY = "Posts";
     String[] TAGS_ARRAY = {"All","Relationships", "Motivation", "Social Skills", "Goal Setting", "Habit Building", "Career", "Health", "Family"};
 }
